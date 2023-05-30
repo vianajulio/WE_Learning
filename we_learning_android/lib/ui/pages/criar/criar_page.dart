1398a8a738247d5ab564db9b12392e971af31391
Gabriel Ferreira da Silva <101992592+Offerreiraa@users.noreply.github.com>
2023-05-30 19:29:04 -0300

    Arrumando o Form

--- we_learning_android/lib/ui/pages/criar/criar_page.dart
+++ we_learning_android/lib/ui/pages/criar/criar_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:we_learning_android/ui/widgets/custom_text.dart';
 import 'package:we_learning_android/ui/pages/criar/criar_user.dart';
+import 'package:we_learning_android/ui/pages/criar/criar_aula.dart';
 
 class CriarPage extends StatelessWidget {
   const CriarPage({super.key});
@@ -12,22 +13,36 @@ class CriarPage extends StatelessWidget {
       child: Scaffold(
         body: SafeArea(
           child: Column(
-            children: const [
-              TabBar(
-                tabs: [
-                  Tab(
-                    text: 'Usuário',
+            children: [
+              Container(
+                height: 50.0,
+                color: Colors.red,
+                alignment: Alignment.center,
+                child: const Text(
+                  "Criar",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
                   ),
-                  Tab(text: 'Aulas'),
-                ],
+                ),
               ),
-              Expanded(
+              Container(
+                color: Colors.red,
+                child: const TabBar(
+                  tabs: [
+                    Tab(
+                      text: 'Usuário',
+                    ),
+                    Tab(text: 'Aulas'),
+                  ],
+                ),
+              ),
+              const Expanded(
                 child: TabBarView(
                   children: [
                     CriarUser(),
-                    Center(
-                      child: Text('2st Tab'),
-                    ),
+                    CriarAula(),
                   ],
                 ),
               )
